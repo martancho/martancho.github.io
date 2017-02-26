4601ffd317fc17c80719e7e69d75eca36aa17e07
Martín Vélez <martin@brandwoodglobal.com>
2017-02-25 17:49:37 -0800

    added folfers for style image and scripts

---
+++ scripts/app.js
@@ -0,0 +1,19 @@
+ fcvar main = function(){
+    $('.dropdown-toggle').click(function(){
+        $('.dropdown-menu').toggle();
+    })
+    
+    $('.arrow-next').click(function(){
+        var currentSlide = $('.active-slide');
+        var nextSlide = currentSlide.next();
+        
+        currentSlide.fadeOut(600).removeClass('active-slide');
+        nextSlide.fadeIn(600).addClass('active-slide');
+
+        if(nextSlide.next == 0){
+        	nextSlide = $('.slide').first();
+        }
+    })
+}
+
+$(document).ready(main);
